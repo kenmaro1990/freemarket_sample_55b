60227acade7c99aaade3e07f903e9334b704ad7e
kenmaro1990 <pinchbeck3006@gmail.com>
2019-10-08 21:20:41 +0900

    usersテーブルに必要なカラムを設定
    usersテーブルに必要なカラムを設定
    住所は別テーブル予定

--- db/migrate/20191007134539_devise_create_users.rb
+++ db/migrate/20191007134539_devise_create_users.rb
@@ -4,8 +4,19 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :nickname, null: false
+      t.string :email, null: false, unique: true
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :first_name_kana, null: false
+      t.string :last_name_kana, null: false
+      t.integer :birthdate_year, null: false
+      t.integer :birthdate_month, null: false
+      t.integer :birthdate_day, null: false
+      t.integer :phone_number, null: false, unique: true
+      t.text :self_introduction
       t.string :encrypted_password, null: false, default: ""
+
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20191007134539) do
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "nickname",                                          null: false
+    t.string   "email",                                             null: false
+    t.string   "first_name",                                        null: false
+    t.string   "last_name",                                         null: false
+    t.string   "first_name_kana",                                   null: false
+    t.string   "last_name_kana",                                    null: false
+    t.integer  "birthdate_year",                                    null: false
+    t.integer  "birthdate_month",                                   null: false
+    t.integer  "birthdate_day",                                     null: false
+    t.integer  "phone_number",                                      null: false
+    t.text     "self_introduction",      limit: 65535
+    t.string   "encrypted_password",                   default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
 
 end
