95af7fddbef6881e8bb25e7aa140c32c3b59a62b
kenmaro1990 <pinchbeck3006@gmail.com>
2019-10-15 20:36:00 +0900

    deviseでのログインビューを作成
    deviseでのログインビューを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#index'
-  resources :users,only: [:index]
+  resources :users
 
   resources :signup do
     collection do
       get 'login'
+      post'login'
+      delete 'login'
       get 'register'
       get 'information'
       get 'phonenumber'
