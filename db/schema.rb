aacfea882a69e9759b2e193faaabbcea3823c717
aivy3 <asami.0323@hotmail.co.jp>
2019-10-21 16:55:58 +0900

    create item_images model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191021023930) do
+ActiveRecord::Schema.define(version: 20191021071957) do
+
+  create_table "item_images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "item_images"
+    t.integer  "item_id",     null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["item_id"], name: "index_item_images_on_item_id", using: :btree
+  end
 
   create_table "items", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                          null: false
@@ -32,4 +40,5 @@ ActiveRecord::Schema.define(version: 20191021023930) do
     t.datetime "updated_at",                    null: false
   end
 
+  add_foreign_key "item_images", "items"
 end
