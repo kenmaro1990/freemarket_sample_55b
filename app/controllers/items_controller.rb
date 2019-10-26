c2142ea8ddf672621747badb1bcd5b0eb78e2b0f
aivy3 <asami.0323@hotmail.co.jp>
2019-10-26 17:06:59 +0900

    modify items_controller.rb
    アイテム一覧表示をIDの昇順から降順に変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,15 +3,15 @@ class ItemsController < ApplicationController
   end
 
   def index
-    @items = Item.includes(:item_images).order('id ASC').limit(10)
-    @ladies_items = Item.includes(:item_images).where(category_id: 1).limit(10).order('id ASC')
-    @mens_items = Item.includes(:item_images).where(category_id: 2).limit(10).order('id ASC')
-    @electrical_appliances = Item.includes(:item_images).where(category_id: 3).limit(10).order('id ASC')
-    @toys = Item.includes(:item_images).where(category_id: 4).limit(10).order('id ASC')
-    @chanel = Item.includes(:item_images).where(brand_id: 1).limit(10).order('id ASC')
-    @louis_vuitton = Item.includes(:item_images).where(brand_id: 2).limit(10).order('id ASC')
-    @supreme = Item.includes(:item_images).where(brand_id: 3).limit(10).order('id ASC')
-    @nike = Item.includes(:item_images).where(brand_id: 4).limit(10).order('id ASC')
+    @items = Item.includes(:item_images).order('id DESC').limit(10)
+    @ladies_items = Item.includes(:item_images).where(category_id: 1).limit(10).order('id DESC')
+    @mens_items = Item.includes(:item_images).where(category_id: 2).limit(10).order('id DESC')
+    @electrical_appliances = Item.includes(:item_images).where(category_id: 3).limit(10).order('id DESC')
+    @toys = Item.includes(:item_images).where(category_id: 4).limit(10).order('id DESC')
+    @chanel = Item.includes(:item_images).where(brand_id: 1).limit(10).order('id DESC')
+    @louis_vuitton = Item.includes(:item_images).where(brand_id: 2).limit(10).order('id DESC')
+    @supreme = Item.includes(:item_images).where(brand_id: 3).limit(10).order('id DESC')
+    @nike = Item.includes(:item_images).where(brand_id: 4).limit(10).order('id DESC')
   end
   
 end
