85f5b3f53fcee6b10de0ac87a590b3111adb1032
jun9130 <jun.nakama1030@gmail.com>
2019-10-30 22:27:45 +0900

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191022145616) do
 ActiveRecord::Schema.define(version: 20191023233209) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
