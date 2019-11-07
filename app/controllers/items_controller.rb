79f59d545eb20700102007dae3d96db1f565f87f
aivy3 <asami.0323@hotmail.co.jp>
2019-11-08 01:05:26 +0900

    update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -70,6 +70,18 @@ class ItemsController < ApplicationController
     @category_grandchildren = Category.find("#{params[:child_id]}").children
   end
 
+  def get_size
+    selected_grandchild = Category.find("#{params[:grandchild_id]}")
+    if related_size_parent = selected_grandchild.sizes[0]
+       @sizes = related_size_parent.children
+    else
+       selected_child = Category.find("#{params[:grandchild_id]}").parent
+       if related_size_parent = selected_child.sizes[0]
+          @sizes = related_size_parent.children 
+       end
+    end
+ end
+
   private
 
   def item_params
@@ -77,7 +89,7 @@ class ItemsController < ApplicationController
       :name, 
       :description, 
       :category_id, 
-      :size,
+      :size_id,
       :price, 
       :condition, 
       :postage, 
