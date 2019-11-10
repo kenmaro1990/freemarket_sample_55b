aa2463ce09d47ac3de76390b3966bea687060b25
aivy3 <asami.0323@hotmail.co.jp>
2019-11-10 23:50:05 +0900

    add get_price routing

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,7 @@ Rails.application.routes.draw do
   resources :items do
     collection do
       get 'transaction'
+      get 'get_price'
       get 'get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', defaults: { format: 'json' }
       get 'get_size', defaults: { format: 'json' }
