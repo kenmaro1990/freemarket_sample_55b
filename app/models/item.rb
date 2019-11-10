949be6e6219a9eccc979a743cb9d873f8a4a49b0
aivy3 <asami.0323@hotmail.co.jp>
2019-11-10 23:50:46 +0900

    modify item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :brand, optional: true
   belongs_to :size, optional: true
-  has_many :item_images
+  has_many :item_images, dependent: :destroy
   accepts_nested_attributes_for :item_images
 
   has_many :comments
