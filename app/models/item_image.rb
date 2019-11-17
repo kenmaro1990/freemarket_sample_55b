06a41724b8fc881a29b626cb53031ee076c6d390
aivy3 <asami.0323@hotmail.co.jp>
2019-11-17 13:03:56 +0900

    add inverse_of: :item_images to item_image.rb

--- app/models/item_image.rb
+++ app/models/item_image.rb
@@ -1,5 +1,5 @@
 class ItemImage < ApplicationRecord
-  belongs_to :item, optional: true
+  belongs_to :item, optional: true, inverse_of: :item_images
   mount_uploader :image, ImageUploader
 
   validates :image, presence: true
