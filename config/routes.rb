3f2899c46bd0b3c37fc448cff3c4f2763d86eedc
kenmaro1990 <pinchbeck3006@gmail.com>
2019-11-17 14:13:10 +0900

    siderテスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, 
-  controllers: { sessions: 'users/sessions', 
+  controllers: { sessions:      
+                      'users/sessions', 
                  omniauth_callbacks: 'users/omniauth_callbacks', 
                  registrations: 'users/registrations'
   }
