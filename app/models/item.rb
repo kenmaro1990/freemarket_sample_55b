2e828fb972740e1a729e59991bd8d8fb66077040
aivy3 <asami.0323@hotmail.co.jp>
2019-11-22 20:04:27 +0900

    add validation to item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,9 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+  belongs_to_active_hash :item_condition
+  belongs_to_active_hash :item_postage
+  belongs_to_active_hash :item_lead_time
   
   belongs_to :user, foreign_key: 'user_id'
   belongs_to :category
@@ -12,5 +15,30 @@ class Item < ApplicationRecord
   has_many :comments
   has_many :messages
   has_many :likes
-  
+
+  validates :item_images,
+    presence: { message: "がありません" }
+  validates :name,
+    presence: true,
+    length: { maximum: 40 }
+  validates :price,
+    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "販売価格は¥300以上¥9,999,999以内で入力してください" }
+  validates :description,
+    presence: true,
+    length: { maximum: 1000 }
+  validates :condition,
+    presence: { message: "を選択して下さい" }
+  validates :postage,
+    presence: { message: "を選択して下さい" }
+  validates :shipping_method,
+    presence: { message: "を選択して下さい" }
+  validates :lead_time,
+    presence: { message: "を選択して下さい" }
+  validates :departure_area,
+    presence: { message: "を選択して下さい" }
+  validates :brand,
+    length: { maximum: 40 }
+  validates :category_id,
+    numericality: { greater_than: 0, message: "を選択して下さい" }
+
 end
