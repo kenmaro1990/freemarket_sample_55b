a8860edbe2a620f82933fb0589cc2394cd242fe0
aivy3 <asami.0323@hotmail.co.jp>
2019-11-24 16:39:36 +0900

    modify items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,10 +28,10 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.includes(:item_images).order('id DESC').limit(10)
-    @ladies_items = Item.includes(:item_images).where(category_id: 1).limit(10).order('id DESC')
-    @mens_items = Item.includes(:item_images).where(category_id: 205).limit(10).order('id DESC')
-    @electrical_appliances = Item.includes(:item_images).where(category_id: 3).limit(10).order('id DESC')
-    @toys = Item.includes(:item_images).where(category_id: 493).limit(10).order('id DESC')
+    @ladies_items = Item.includes(:item_images).where(category_id: 3).limit(10).order('id DESC')
+    @mens_items = Item.includes(:item_images).where(category_id: 207).limit(10).order('id DESC')
+    @electrical_appliances = Item.includes(:item_images).where(category_id: 947).limit(10).order('id DESC')
+    @toys = Item.includes(:item_images).where(category_id: 728).limit(10).order('id DESC')
     @chanel = Item.includes(:item_images).where(brand_id: 1).limit(10).order('id DESC')
     @louis_vuitton = Item.includes(:item_images).where(brand_id: 3).limit(10).order('id DESC')
     @supreme = Item.includes(:item_images).where(brand_id: 4).limit(10).order('id DESC')
